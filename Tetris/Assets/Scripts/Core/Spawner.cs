0378dc0e67280b28025c850e914dc35753a730c5
Thomas Tran <tran.th.thomas@gmail.com>
2019-12-30 23:51:17 -0500

    Add methods to Spawner class to spawn random shapes

--- Tetris/Assets/Scripts/Core/Spawner.cs
+++ Tetris/Assets/Scripts/Core/Spawner.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public Shape[] m_allShapes;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    Shape GetRandomShape()
+    {
+        int i = Random.Range(0, m_allShapes.Length);
+        if (m_allShapes[i])
+        {
+            return m_allShapes[i];
+        }
+        else
+        {
+            Debug.LogWarning("WARN: Invalid shape in spawner!");
+            return null;
+        }
+    }
+
+    public Shape SpawnShape()
+    {
+        Shape shape = null;
+        shape = Instantiate(GetRandomShape(), transform.position, Quaternion.identity) as Shape;
+        if (shape)
+        {
+            return shape;
+        }
+        else
+        {
+            return null;
+        }
+
     }
 }
