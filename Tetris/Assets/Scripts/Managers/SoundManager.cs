e41dc3603ee792e5bf1ca8a812bca928c7756aa8
Thomas Tran <tran.th.thomas@gmail.com>
2019-12-31 15:19:58 -0500

    Add SoundManager class

---
+++ Tetris/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public bool m_musicEnabled = true;
+    public bool m_fxEnabled = true;
+    [Range(0,1)]
+    public float m_musicVolumes = 1.0f;
+    [Range(0,1)]
+    public float m_fxVolume = 1.0f;
+    public AudioClip m_clearRowSound;
+    public AudioClip m_moveSound;
+    public AudioClip m_dropSound;
+    public AudioClip m_gameOverSound;
+    public AudioClip m_backgroundMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
