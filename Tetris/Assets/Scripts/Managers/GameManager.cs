aa333c7ca1e15198113b1786ff6192e00b581b7b
Thomas Tran <tran.th.thomas@gmail.com>
2019-12-31 02:04:45 -0500

    Player input :)

--- Tetris/Assets/Scripts/Managers/GameManager.cs
+++ Tetris/Assets/Scripts/Managers/GameManager.cs
@@ -7,12 +7,22 @@ public class GameManager : MonoBehaviour
     Board m_gameBoard;
     Spawner m_spawner;
     Shape m_activeShape;
-    float m_dropInterval = 0.25f;
+    float m_dropInterval = 0.5f;
     float m_timeToDrop;
+    float m_timeToNextKeyLeftRight;
+    [Range(0.02f, 1f)]
+    public float m_keyRepeatRateLeftRight = 0.15f;
+    float m_timeToNextKeyDown;
+    [Range(0.01f, 1f)]
+    public float m_keyRepeatRateDown = 0.02f;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_timeToDrop = Time.time;
+        m_timeToNextKeyLeftRight = Time.time + m_keyRepeatRateLeftRight;
+        m_timeToNextKeyDown = Time.time + m_keyRepeatRateDown;
+
         m_gameBoard = GameObject.FindWithTag("Board").GetComponent<Board>();
         m_spawner = GameObject.FindWithTag("Spawner").GetComponent<Spawner>();
 
@@ -38,32 +48,62 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!m_gameBoard || !m_spawner)
+        if (!m_gameBoard || !m_spawner || !m_activeShape)
         {
             return;
         }
-        // Drop in specified increments
-        if (Time.time > m_timeToDrop)
+        PlayerInput();
+    }
+
+    void PlayerInput()
+    {
+        if (Input.GetButton("MoveRight") && (Time.time > m_timeToNextKeyLeftRight) || Input.GetButtonDown("MoveRight"))
         {
-            m_timeToDrop = Time.time + m_dropInterval;
-            if (m_activeShape)
+            m_timeToNextKeyLeftRight = Time.time + m_keyRepeatRateLeftRight;
+            m_activeShape.MoveRight();
+            if (!m_gameBoard.IsValidPosition(m_activeShape))
             {
-                m_activeShape.MoveDown();
-
-                // Validate the shape's position within board
-                if (!m_gameBoard.IsValidPosition(m_activeShape))
-                {
-                    // Shape lands and registers in grid
-                    m_activeShape.MoveUp();
-                    m_gameBoard.StoreShapeInGrid(m_activeShape);
-
-                    // Spawn a new shape
-                    if (m_spawner)
-                    {
-                        m_activeShape = m_spawner.SpawnShape();
-                    }
-                 }
+                m_activeShape.MoveLeft();
             }
         }
+        else if (Input.GetButton("MoveLeft") && (Time.time > m_timeToNextKeyLeftRight) || Input.GetButtonDown("MoveLeft"))
+        {
+            m_timeToNextKeyLeftRight = Time.time + m_keyRepeatRateLeftRight;
+            m_activeShape.MoveLeft();
+            if (!m_gameBoard.IsValidPosition(m_activeShape))
+            {
+                m_activeShape.MoveRight();
+            }
+        }
+        else if (Input.GetButtonDown("Rotate"))
+        {
+            m_activeShape.RotateRight();
+            if (!m_gameBoard.IsValidPosition(m_activeShape))
+            {
+                m_activeShape.RotateLeft();
+            }
+        }
+        else if (Input.GetButton("MoveDown") && (Time.time > m_timeToNextKeyDown) || (Time.time > m_timeToDrop))
+        {
+            m_timeToNextKeyDown = Time.time + m_keyRepeatRateDown;
+            m_timeToDrop = Time.time + m_dropInterval;
+            m_activeShape.MoveDown();
+
+            // Validate the shape's position within board
+            if (!m_gameBoard.IsValidPosition(m_activeShape))
+            {
+                LandShape();
+            }
+        }
+    }
+
+    void LandShape()
+    {
+        // No input delay after shape lands
+        m_timeToNextKeyLeftRight = Time.time;
+        m_timeToNextKeyDown = Time.time;
+        m_activeShape.MoveUp();
+        m_gameBoard.StoreShapeInGrid(m_activeShape);
+        m_activeShape = m_spawner.SpawnShape();
     }
 }
