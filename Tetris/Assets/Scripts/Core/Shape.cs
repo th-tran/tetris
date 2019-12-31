6a250d43a6a8d405b1e19f003401ec16ce87dcc7
Thomas Tran <tran.th.thomas@gmail.com>
2019-12-30 23:17:34 -0500

    Add move and rotate methods to Shape class

--- Tetris/Assets/Scripts/Core/Shape.cs
+++ Tetris/Assets/Scripts/Core/Shape.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
+    public bool m_canRotate = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void Move(Vector3 moveDirection)
+    {
+        transform.position += moveDirection;
+    }
+
+    public void MoveLeft()
+    {
+        Move(new Vector3(-1, 0, 0));
+    }
+
+    public void MoveRight()
+    {
+        Move(new Vector3(1, 0, 0));
+    }
+
+    public void MoveDown()
+    {
+        Move(new Vector3(0, -1, 0));
+    }
+
+    public void MoveUp()
+    {
+        Move(new Vector3(0, 1, 0));
+    }
+
+    public void RotateRight()
+    {
+        if (m_canRotate)
+        {
+            transform.Rotate(0, 0, -90);
+        }
+    }
+
+    public void RotateLeft()
+    {
+        if (m_canRotate)
+        {
+            transform.Rotate(0, 0, 90);
+        }
     }
 }
