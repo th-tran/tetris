8fbe6813b8ec1691e4b217bb76392c860885c675
Thomas Tran <tran.th.thomas@gmail.com>
2020-01-05 17:48:41 -0500

    Set startup resolution and remove launch config (to be added as settings in game)

---
+++ Tetris/Assets/Scripts/Utility/SetStartupResolution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartupResolution : MonoBehaviour
+{
+    public int m_width = 405;
+    public int m_height = 720;
+    public bool m_isFullScreen = false;
+    public int m_refreshRate = 60;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("FirstStartup") != 1)
+        {
+            PlayerPrefs.SetInt("FirstStartup", 1);
+            Screen.SetResolution(m_width, m_height, m_isFullScreen, m_refreshRate);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt("FirstStartup", 0);
+    }
+}
